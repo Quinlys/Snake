31aec6daa1908721515cf60f3a032c98fc9e2d35
Igor Yakymiv <yakymiv.ig@gmail.com>
2018-06-16 18:52:09 +0300

    Upload game

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour {
+
+	public float timing;
+	public bool isPaused;
+	public GameObject menu;
+
+	void Start () {
+	}
+
+	void Update () {
+		Time.timeScale = timing;
+		if(Input.GetKeyDown(KeyCode.Escape) && isPaused == false)
+		{	
+			isPaused = true;
+		}
+		else if (Input.GetKeyDown(KeyCode.Escape) && isPaused == true)
+		{
+			isPaused = false;
+		}
+
+		if(isPaused == true )
+		{
+			timing = 0;
+			menu.SetActive(true);
+		}
+		else if(isPaused == false)
+		{
+			timing = 1;
+			menu.SetActive(false);
+		}
+	}
+
+	public void QuitButton() {
+		Application.Quit();
+	}
+}
